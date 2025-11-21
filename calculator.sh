af1a7e9f378434910361f58c5182d37899c04776
배진영 <112489573+qowlsdud@users.noreply.github.com>
2025-11-22 00:06:34 +0900

    Delete calculator.sh

--- calculator.sh
+++
@@ -1,7 +0,0 @@
-NUM1=$1
-NUM2=$2
-
-echo "덧셈: $NUM1 + $NUM2 = $((NUM1 + NUM2))"
-echo "뺄셈: $NUM1 - $NUM2 = $((NUM1 - NUM2))"
-echo "곱셈: $NUM1 * $NUM2 = $((NUM1 * NUM2))"
-echo "나눗셈: $NUM1 / $NUM2 = $((NUM1 / NUM2))"
